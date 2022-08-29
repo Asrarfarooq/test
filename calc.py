1c655f352f50a74f4d6862f8ef431399aaf30e52
Asrar <81701822+Asrarfarooq@users.noreply.github.com>
2022-08-29 13:21:42 -0500

    Create calc.py
    V.01 Basic Calc

---
+++ calc.py
@@ -0,0 +1,5 @@
+#Writing a basic program to add 2 numbers for my EECS 448 Lab-01
+first_num=int(input("Enter First Number: "))
+second_num=int(input("Enter Second Number: "))
+Result=first_num+second_num
+print("Result: "+str(Result))
